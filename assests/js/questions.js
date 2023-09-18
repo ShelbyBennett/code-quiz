b19b2c4a243cef063a7dc5e0b826ff0abad50ebe
Shelby Bennett <shelshel@Shelbys-MBP.lan>
2023-09-18 13:45:56 -0400

    putting more questions on the quiz

--- assests/js/questions.js
+++ assests/js/questions.js
@@ -22,4 +22,36 @@ correct:".container"
     answer4:"<figcaption>",
     correct:"<figcaption>"
     },
+    {
+        question:"What HTML stand for?",
+        answer1:"Hyper Trainer Marking Language",
+        answer2:"Hyper Text Marketing Language",
+        answer3:"Hyper Text Markup Language",
+        answer4:"Hyper Text Markup Leveler",
+        correct:"Hyper Text Markup Langauage"
+        },
+        {
+            question:"How do you leave a comment in HTML?",
+            answer1:"<!--comment-->",
+            answer2:"<!-comment->",
+            answer3:"<--comment-->",
+            answer4:"<-comment->",
+            correct:"<!--comment-->"
+            },
+            {
+                question:"What is an example of a web API?",
+                answer1:"CSS",
+                answer2:"HTML",
+                answer3:"Bootstrap",
+                answer4:"All of the above",
+                correct:"Bootstrap"
+                },
+                {
+                    question:"What's the semantic html tag for a img caption?",
+                    answer1:"<figure>",
+                    answer2:"<img>",
+                    answer3:"<header>",
+                    answer4:"<figcaption>",
+                    correct:"<figcaption>"
+                    },
 ]
